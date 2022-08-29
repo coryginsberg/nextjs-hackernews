6cdeb815a0884095c2386be99a774dee425f0dcc
Cory Ginsberg <cory.ginsberg1@gmail.com>
2022-08-29 11:28:09 -0500

    Initial Commit

---
+++ pages/api/hackerNewsApi.ts
@@ -0,0 +1,43 @@
+const BASE_URL = "https://hacker-news.firebaseio.com/v0";
+
+export interface PostInfo {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: Array<number>;
+  score: number;
+  text: string;
+  time: number;
+  title: string;
+  type: string;
+}
+
+const awaitJson = (responses: any[]) =>
+  Promise.all(
+    responses.map((response) => {
+      if (response.ok) return response.json();
+      throw new Error(response.statusText);
+    }),
+  );
+
+export async function genTopStories(
+  limit: number | null = null,
+): Promise<Array<PostInfo>> {
+  return await fetch(new URL(`${BASE_URL}/topstories.json`))
+    .then((response) => response.json())
+    .then((data) => {
+      if (limit != null) {
+        data = data.slice(0, limit);
+      }
+      let promises = [];
+      for (const dat of data) {
+        promises.push(fetch(`${BASE_URL}/item/${dat}.json`));
+      }
+      return Promise.all(promises);
+    })
+    .then(awaitJson)
+    .catch((error) => {
+      console.error(error);
+      return [];
+    });
+}
---
+++ pages/main.tsx
@@ -0,0 +1,65 @@
+import React, { Component, useEffect, useState } from "react";
+import styles from "../styles/Main.module.css";
+import { PostInfo } from "./api/hackerNewsApi";
+import * as HackerNewsApi from "./api/hackerNewsApi";
+
+type Props = {};
+
+export default function Main({}: Props) {
+  const [topResults, setTopResults] = useState<PostInfo[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    (async () => {
+      const temp = await HackerNewsApi.genTopStories(10);
+      setTopResults(temp);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!topResults) return <p>No data</p>;
+
+  const subhead = (result: PostInfo) => {
+    const time = calculateHowLongAgo(result.time);
+    return (
+      <h4>
+        {result.score} points by {result.by} {time} ago | {result.descendants}{" "}
+        comments
+      </h4>
+    );
+  };
+
+  const calculateHowLongAgo = (time: number) => {
+    const date = new Date(time * 1000);
+    const currentTime = new Date();
+    console.warn(date);
+    if (currentTime.getDay() - date.getDay() > 0) {
+      return Math.floor(currentTime.getDay() - date.getDay()) + " days";
+    } else if (currentTime.getHours() - date.getHours() > 0) {
+      return Math.floor(currentTime.getHours() - date.getHours()) + " hours";
+    } else if (currentTime.getMinutes() - date.getMinutes() > 0) {
+      return (
+        Math.floor(currentTime.getMinutes() - date.getMinutes()) + " minutes"
+      );
+    } else {
+      return (
+        Math.floor(currentTime.getSeconds() - date.getSeconds()) + " seconds"
+      );
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      {topResults.map((result, key) => (
+        <div key={result.id}>
+          <h2 className="title">
+            {key + 1}. {result.title}
+          </h2>
+          {subhead(result)}
+        </div>
+      ))}
+    </div>
+  );
+}
